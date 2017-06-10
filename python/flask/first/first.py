9edebe18112520c1ba3ff66d6fd4eb0f40d379eb
bdastur <bdastur@gmail.com>
2017-06-10 15:27:40 -0700

    Add additional examples of response types

--- python/flask/first/first.py
+++ python/flask/first/first.py
@@ -3,6 +3,8 @@
 
 
 from flask import Flask
+from flask import make_response
+from flask import redirect, abort
 
 app = Flask(__name__)
 
@@ -13,6 +15,24 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return "<h1>Hello %s</h1>" % name
+
+@app.route('/cookie/')
+def setcookie():
+    response = make_response(
+    '<h1>Set Cookie</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.route('/redirectme')
+def redirect_test():
+    return redirect('/')
+
+
+@app.route('/abortonodd/<number>')
+def abort_test(number):
+    if int(number) % 2 != 0:
+        abort(500)
+    return '<h2>Numer %s is even</h2>' % number
 
 
 def main():
