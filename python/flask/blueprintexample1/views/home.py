51add2a609c1550bf8dbe41a558716d52b0a9cbd
bdastur <bdastur@gmail.com>
2017-06-23 15:31:50 -0700

    Add tempaltes to the blueprintexample1 dir

--- python/flask/blueprintexample1/views/home.py
+++ python/flask/blueprintexample1/views/home.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, render_template
+from flask import request
 
 home = Blueprint('home', __name__,
                  template_folder="templates",
                  static_folder="static")
+
 
 @home.route("/")
 def homeindex():
@@ -15,3 +17,17 @@ def homeindex():
 def home_username(username):
     print "username: ", username
     return render_template('user.html')
+
+@home.route("/userinput", methods=['POST', 'GET'])
+def userinput():
+    obj = {}
+    obj['method'] = "GET"
+    if request.method == "POST":
+        print "POST method"
+        print "Form info: ", request.form
+        print "email: ", request.form['email']
+        obj['method'] = "POST"
+    else:
+        print "GET METHOD"
+
+    return render_template('userinput.html',obj=obj)
