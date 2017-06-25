40d24bcdb66fcbe591231c5a3d10dd8c496b51a6
bdastur <bdastur@gmail.com>
2017-06-25 07:25:03 -0700

    Form handler

--- python/flask/howtos/views/forms.py
+++ python/flask/howtos/views/forms.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Example of managing forms.
-Taking input from forms
-'''
 
-from flask import Blueprint, render_template, request
+Taking input from forms
+"""
+
+from flask import Blueprint, render_template, request, jsonify
 
 forms = Blueprint('forms', __name__,
                  template_folder="templates", static_folder="static")
@@ -16,5 +17,16 @@ forms = Blueprint('forms', __name__,
 def index():
     print "Request method: ", request.method
     print "Request.form: ", request.form
+    obj = {}
+    if request.method == "POST":
+        obj['email'] = request.form['email']
+
+    return render_template('forms.html',
+                            obj=obj)
+
+@forms.route("/formhandle", methods=["GET", "POST"])
+def formHandler():
+    obj = {}
+    print "Form handler. reqeust method: ", request.method
+    return jsonify(obj)
     
-    return render_template('forms.html')
