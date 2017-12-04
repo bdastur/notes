a20483f1facc406ce3a0e23c4aa400ba46f58db3
Behzad Dastur <bdastur@gmail.com>
2017-12-04 15:02:44 -0800

    Add wrapper to invoke oc and docker commands.

--- kubernetes/simple-web-app/app.sh
+++ kubernetes/simple-web-app/app.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 
 operation=
-valid_operations=("build" "push")
+image=${APP_DEFAULT_IMAGE}
+deploymentconfigfile=
+
+valid_operations=("build" "push" "deploy")
 
 function usage {
     echo "Usage: app.sh <operation> [options]"
     echo "-----------------------------------"
+    echo "Allowed Operations: ${valid_operations[@]}"
+    echo "Examples: "
+    echo ""
+    echo "build a docker image:"
+    echo "./app build ."
+    echo "./app build -i registry.abc.acme.com/alpine:1.0"
+    echo ""
+    echo "deploy a oc template:"
+    echo "./app deploy openshift/deploymentconfig.yaml"
+    echo ""    
+    
     exit 1
 }
 
 function validate_operation {
   #statements
   local operation=$1
+  valid=false
   echo "operation: $operation"
   for x in ${valid_operations[@]}; do
-    echo "$x"
+    if [[ $x == "$operation" ]]; then
+        valid=true
+    fi
   done
+
+  if [[ $valid == false ]]; then
+    echo "Invalid operation $operation. Must be one of: '${valid_operations[@]}'"
+    exit 1
+  fi
+}
+
+function app_operation_build {
+    local args=$1
+    echo "image: $image, args: $args"
+    docker build --tag=$image $args
+}
+
+function app_operation_deploy {
+    echo "dep config: $deploymentconfigfile"
+    formatimage=$(echo $image | sed 's/\//\\\//g')
+    echo "sed  's/APP_DEFAULT_IMAGE/${formatimage}/' $deploymentconfigfile"
+    cat $deploymentconfigfile | sed "s/APP_DEFAULT_IMAGE/${formatimage}/" > /tmp/tempdeployment.yaml
+    oc process -f /tmp/tempdeployment.yaml | oc apply -f - 
 }
 
 
@@ -39,7 +75,28 @@ readonly COMMANDLINE="$*"
 
 # Get service type.
 operation=$1
+image=${APP_DEFAULT_IMAGE}
+
 validate_operation $operation
 shift
 
+CMD_OPTIONS="i:f:"
 
+while getopts ${CMD_OPTIONS} option; do
+  case $option in
+      f)
+          deploymentconfigfile=$OPTARG
+          ;;
+      i)
+          image=$OPTARG
+          shift
+          shift
+          ;;
+      :) echo "Error option needs argument";; 
+      *) echo "Error: Invalid option";;
+  esac       
+done
+
+echo "deployment config: $deploymentconfigfile"
+echo "args remaining: $*"
+app_operation_${operation} $*
