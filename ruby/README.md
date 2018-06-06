7233837d97a26c72838aa5f8c2261b5b859ea219
Behzad Dastur <behzad.dastur@gmail.com>
2018-06-06 09:07:41 -0700

    Add Ruby notes.

---
+++ ruby/README.md
@@ -0,0 +1,22 @@
+# Ruby Notes:
+
+## Variables in Ruby:
+* Local Variables:
+  * Scope is local to a method.
+  * Not available outside the method.
+  * Beging witha lowercase letter or _
+
+* Instance Variables:
+  * Available across methods for any particular instance or object.
+  * Instance variables are preceded by a (@) followed by the variable name.
+
+* Class Variables:
+  * Belong to the class.
+  * Available across different objects.
+  * Preceded by a (@@) followed by the variable name.
+
+* Global Variables:
+  * Available globally across classes.
+  * Preceded by a $ sign.
+
+
---
+++ ruby/examples/basic.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/ruby
+
+# A print statement
+puts "Hello Ruby"
+
+# Data types and variables.
+## Declaring a variable.
+simple_str = "This is a test string"
+simple_num = 434
+
+puts "Simple String: #{simple_str}, Number: #{simple_num}\n"
+
+
+
+# Defining a class.
+class Employee
+    # Specifying an attr_reader or attr_accessor,
+    # will create a getter for the given variable 'employee_id'
+    attr_reader :employee_id
+
+    @@employee_count = 0
+    def initialize (id, name)
+        @employee_name = name
+        @employee_id = id
+        @@employee_count += 1
+
+    end
+end
+
+# Initialize a new employee object.
+employee_1 = Employee.new(2333, "behzad")
+
+# One way of accessing instance variables.
+emp_name = employee_1.instance_variable_get(:@employee_name)
+puts "Name: #{emp_name}"
+
+# Another way. In this case we had setup a getter using attr_reader
+puts "Employee id: #{employee_1.employee_id}"
+
+# Get class variables.
+emp_count = Employee.class_variable_get(:@@employee_count)
+puts "count: #{emp_count}"
