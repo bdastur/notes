5ac702f7bbf5f66a8e9004209efe9ba8ad476cac
Behzad Dastur <bdastur@gmail.com>
2020-11-24 15:00:15 -0800

    Update promethesus run parameters

---
+++ prometheus/prometheus/README.md
@@ -0,0 +1,10 @@
+
+## Build the prometheus docker image
+```
+docker build --tag bdastur/prometheus:2.21.0 .
+```
+
+## Running prometheus container.
+```
+./run_prometheus.sh
+```
--- prometheus/prometheus/start.sh
+++ prometheus/prometheus/start.sh
@@ -3,6 +3,9 @@ echo "Prometheus Start script"
 
 echo "$@"
 
+# Look for user provided config file in /pconfig
+cp /pconfig/prometheus.yml /etc/prometheus/prometheus.yml
+
 #./start.sh --config.file /etc/prometheus/prometheus.yml --storage.tsdb.path /prometheus_data/ --web.console.libraries /etc/prometheus/console_libraries/ --web.console.templates /etc/prometheus/consoles
 
 /usr/local/bin/prometheus $@
