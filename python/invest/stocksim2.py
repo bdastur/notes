b99a463cd272d7dc2d307fa74db1f4a24e66bdaf
Behzad Dastur <bdastur@gmail.com>
2020-12-30 14:55:33 -0800

    Add logic with actual historical price data

--- python/invest/stocksim2.py
+++ python/invest/stocksim2.py
@@ -11,7 +11,7 @@ def parse_historical_data(symbol):
     hist_data = []
     filename = "%s.csv" % symbol
     filepath = os.path.join("/tmp", filename)
-    
+
     if not os.path.exists(filepath):
         print("File does not exist")
         return
@@ -46,7 +46,7 @@ def get_new_price(current_price):
     return price_fluctuation
 
 def get_price_difference(old_price, new_price):
-    return (new_price - old_price)
+    return  (new_price - old_price)
 
 def sell_stock(accumulated_stock, sell_price, sell_quantity):
     #print("Selling: %d, %d, %d" % (accumulated_stock, sell_price, sell_quantity))
@@ -73,9 +73,72 @@ def buy_stock(balance, stock_price, buy_quantity):
         return(balance, buy_quantity)
 
     cost_basis = int(stock_price) * int(buy_quantity)
-    balance = balance - cost_basis 
+    balance = balance - cost_basis
     return (balance, buy_quantity)
-    
+
+
+def run_stock_iterations(symbol):
+    balance = 10000
+    buy_threshold = -1.0
+    sell_threshold = 2.0
+    stock_price = 240
+    buy_batch = 50
+    sell_batch = 50
+    shares_accumulated = 0
+    transactions = 100000
+    cumulative_rise = 0
+    cumulative_drop = 0
+
+    filename = "%s.csv" % symbol
+    filepath = os.path.join("/tmp", filename)
+
+    if not os.path.exists(filepath):
+        print("File does not exist")
+        return
+
+    print("[Date]       Old Stock Price  New Stock Price  %Change  Operation    Balance  Shares")
+    initiate = True
+    for line in open(filepath, 'r'):
+        operation = "NA"
+        old_price = stock_price
+
+        columns = line.split(",")
+        # Get Closing price:
+        try:
+            stock_price = float(columns[4])
+        except ValueError:
+            continue
+        date = columns[0]
+        if initiate:
+            initiate = False
+            old_price = stock_price
+            continue
+
+        change = get_price_difference(old_price, stock_price)
+        percent_change = float(float(change)/float(old_price) * 100)
+        if percent_change < 0:
+            cumulative_rise = 0
+            if percent_change < buy_threshold:
+                balance, bought = buy_stock(balance, stock_price, buy_batch)
+                shares_accumulated += bought
+                operation = "buy"
+        else:
+            cumulative_rise += percent_change
+            if (percent_change > sell_threshold) or (cumulative_rise > sell_threshold):
+                #print("%f is > %f, so we sell" % (percent_change, sell_threshold))
+                gain, shares_accumulated = sell_stock(shares_accumulated, stock_price, sell_batch)
+                balance += gain
+                operation = "sell"
+
+        if shares_accumulated > 0:
+            current_value = balance + (int(stock_price) * int(shares_accumulated))
+        else:
+            current_value = balance
+
+        print("[%s] %10f %10f %22f %5s %14d %6d %5d" % \
+                (date, old_price, stock_price, percent_change, operation, balance, shares_accumulated, current_value))
+
+
 
 
 def run_iterations():
@@ -83,7 +146,7 @@ def run_iterations():
     buy_threshold = -1.0
     sell_threshold = 2.0
     stock_price = 240
-    buy_batch = 50 
+    buy_batch = 50
     sell_batch = 50
     shares_accumulated = 0
     transactions = 100000
@@ -111,7 +174,7 @@ def run_iterations():
                 gain, shares_accumulated = sell_stock(shares_accumulated, stock_price, sell_batch)
                 balance += gain
                 operation = "sell"
-       
+
         if shares_accumulated > 0:
             current_value = balance + (int(stock_price) * int(shares_accumulated))
         else:
@@ -122,6 +185,7 @@ def run_iterations():
 
 def main():
     run_iterations()
+    #run_stock_iterations("SPY")
     #parse_historical_data("SPY")
 
 
