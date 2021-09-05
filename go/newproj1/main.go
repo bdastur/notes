7e88023bb9137a68b7541a6f483bfe2dc1741f6c
Behzad Dastur <bdastur@gmail.com>
2021-09-05 09:04:27 -0700

    Add Makefile for go template

--- go/newproj1/main.go
+++ go/newproj1/main.go
@@ -1,11 +1,13 @@
 package main
 import "github.com/bdastur/newproj1/cmd"
+import "github.com/bdastur/newproj1/internal/utils"
 
 import "fmt"
 import "os"
 
 func main() {
     fmt.Printf("Go Main! \n")
+    utils.LogMessage()
     if err := cmd.NewProjCommand().Execute(); err != nil {
         os.Exit(1)
     }
