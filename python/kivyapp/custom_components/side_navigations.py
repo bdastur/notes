82246b3ead4c2a2f26c3c6c27cf534e666ea0625
Behzad Dastur <bdastur@gmail.com>
2023-01-26 05:54:44 -0800

    Add side nav example in kivy

--- python/kivyapp/custom_components/side_navigations.py
+++ python/kivyapp/custom_components/side_navigations.py
@@ -4,61 +4,52 @@
 
 from kivymd.uix.button import MDRectangleFlatIconButton 
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
-from kivy.lang.builder import Builder
 from kivy.utils import get_color_from_hex as C
 
-
 KV = """
-#:import C kivy.utils.get_color_from_hex
-
-<MenuButton>:
-    #size_hint: [1, 1]
-    size_hint_x: 1
-    size_y: 50
-    icon_color: "orange"
-    md_bg_color: C("#432343")
-
-<MyBox>:
-
-#<SideNavLayout>:
-#    orientation: "vertical"
-
+BoxLayout:
+    orientation: "vertical"
+    Button:
+        text: "Testing"
+    Button:
+        text: "Testing"
 """
-#Builder.load_string(KV)
-
-class MenuButton(MDRectangleFlatIconButton):
-    pass
-
-
-class MyBox(BoxLayout):
-    def __init__(self, **kwargs):
-        super(MyBox, self).__init__(**kwargs)
-        buttonOne = ObjectProperty()
-        self.buttonOne.bind(on_press=self.buttonClickHandler)
-
-    def buttonClickHandler(self, btnInstance):
-        print("Button %s clicked: ", btnInstance.text)
 
 
 class SideNavLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(SideNavLayout, self).__init__(**kwargs)
         self.orientation = "vertical"
-        self.pos_hint = {"center_x": 0.5, "top": 1.0}
-
+        self.background_color = C("#334433")
+        self.color = C("#223344")
+        self.padding = 0
+        self.spacing = 5
         self.navItems = []
+        self.itemProperties = {}
+        self.setItemProperties()
 
-    def addNavItem(self, **navItemOption):
-        text = navItemOption["text"]
-        icon = navItemOption["icon"]
 
-        navItem = MDRectangleFlatIconButton(text=text, icon=icon)
+    def setItemProperties(self, height=60, md_bg_color="#434343", icon_color="#764333", 
+                          text_color="#ffffff"):
+        self.itemProperties["height"] = height
+        self.itemProperties["md_bg_color"] = C(md_bg_color)
+        self.itemProperties["icon_color"] = C(icon_color)
+        self.itemProperties["text_color"] = C(text_color)
+
+
+    def addNavItem(self, text, icon=None):
+
+        if icon is not None:
+            navItem = MDRectangleFlatIconButton(text=text, icon=icon)
+        else:
+            navItem = MDRectangleFlatIconButton(text=text)
         navItem.size_hint_x = 1
-        navItem.size_y = 60
-        navItem.md_bg_color = C("#434323")
-        navItem.icon_color = C("#FF5667")
         navItem.pos_hint = {"left": 1, "top": 1}
+
+        navItem.height = self.itemProperties["height"] = 60
+        navItem.md_bg_color = self.itemProperties["md_bg_color"]
+        navItem.icon_color = self.itemProperties["icon_color"]
+        
 
         self.add_widget(navItem)
         self.navItems.append(navItem)
