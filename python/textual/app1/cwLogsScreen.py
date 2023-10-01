7ae7d09685162a34c91bdf970642c5a1181905ab
Behzad Dastur <bdastur@gmail.com>
2023-10-01 14:17:18 -0700

    Update cwlogs screen, message passing

--- python/textual/app1/cwLogsScreen.py
+++ python/textual/app1/cwLogsScreen.py
@@ -3,9 +3,10 @@
 
 from textual.app import App, ComposeResult
 from textual.screen import Screen
+from textual.message import Message
 from textual.containers import Container, Horizontal, VerticalScroll, ScrollableContainer
 from textual.widgets import Header, Static, Placeholder, Input, Label, Button, Collapsible
-import app1libs.environments as environments                                                                                                                                                            
+import app1libs.environments as environments
 
 
 class RegionSelectButton(Button):
@@ -13,9 +14,8 @@ class RegionSelectButton(Button):
 
     def on_mount(self):
         self.originalLabel = self.label;
-    
+
     def on_button_pressed(self, event) -> None:
-        print("BRD: on toggle button ", type(event))
         if not self.toggleOn:
             self.toggleOn = True
             self.classes = "btnToggleOn"
@@ -26,20 +26,25 @@ class RegionSelectButton(Button):
             self.label = self.originalLabel;
         print(self.label)
 
-class RegionSelector(Collapsible):    
+class RegionSelector(Collapsible):
     envObjs = []
     def compose(self) -> ComposeResult:
         envObj = environments.Environments(envFile="./configs/environments.json")
         awsEnvs = envObj.getAwsEnvironments()
-    
-        with Collapsible(title="Environment Selection", 
+
+        with Collapsible(title="Environment Selection",
                          classes="collapse01", collapsed=True):
             for env in awsEnvs:
+                envData = {}
+                envData['region'] = envObj.getAwsEnvironmentRegion(env)
+                envData['profile'] = envObj.getAwsEnvironmentProfile(env)
+
                 envString = "%s (%s)" %\
                     (envObj.getAwsEnvironmentRegion(env),envObj.getAwsEnvironmentProfile(env))
-                obj = RegionSelectButton(envString, id=env, classes="btnToggleOff")
-                self.envObjs.append(obj)
-                yield obj
+                envData['obj'] = RegionSelectButton(envString, id=env,
+                                         classes="btnToggleOff")
+                self.envObjs.append(envData)
+                yield envData['obj']
 
 
 class SideMenuBar(VerticalScroll):
@@ -49,24 +54,22 @@ class SideMenuBar(VerticalScroll):
         yield Placeholder("Menu item one", classes="menuItems")
         yield Placeholder("Menu item two", classes="menuItems")
         yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
-        yield Placeholder("Menu item two", classes="menuItems")
+
 
 
 
 class InputArea(Container):
     def compose(self) -> ComposeResult:
         self.input = Input(placeholder="Text input", id="input01")
-        yield self.input
         self.submitButton = Button("Submit", id="button01")
+
+        yield self.input
         yield self.submitButton
+
+    def on_button_pressed(self, event: Button.Pressed):
+        print("InputArea: Submit button pressed: ", event.button.id)
+        #self.post_message(self.Notify(self.input.value))
+
 
 
 class ResultArea(ScrollableContainer):
@@ -81,7 +84,7 @@ class ResultArea(ScrollableContainer):
         yield Placeholder("Result Item seven", classes="resultItems")
         yield Placeholder("Result Item eight", classes="resultItems")
         yield Placeholder("Result Item nine", classes="resultItems")
-        # Note that since Result area is a grid, it does not scroll. 
+        # Note that since Result area is a grid, it does not scroll.
         # we should probably put this within a vertical scroll
         yield Placeholder("Result Item ten", classes="resultItems")
         yield Placeholder("Result Item eleven", classes="resultItems")
@@ -91,14 +94,29 @@ class ResultArea(ScrollableContainer):
 
 class ContentArea(Container):
     def compose(self) -> ComposeResult:
-        yield InputArea(id="inputArea")
-        yield ResultArea(id="resultArea")
+        self.inputArea = InputArea(id="inputArea")
+        yield self.inputArea
+
+        self.resultArea = ResultArea(id="resultArea")
+        yield self.resultArea
+
+    def on_button_pressed(self, event: Button.Pressed):
+        print("Content Area button pressed: ", event.button.id, self.inputArea.input.value)
 
 
 class CWLogsScreen(Screen):
     def compose(self) -> ComposeResult:
-        yield SideMenuBar(id="sideMenubar")
-        yield ContentArea(id="contentArea")
+        self.sideMenubar = SideMenuBar(id="sideMenubar")
+        self.contentArea = ContentArea(id="contentArea")
+
+        yield self.sideMenubar
+        yield self.contentArea
+
+    def on_button_pressed(self, event: Button.Pressed):
+        print("CWLogs screen button press: ", event.button.id)
+        print("CW Logs screen: ", self.sideMenubar.regionSelector.envObjs)
+        for env in self.sideMenubar.regionSelector.envObjs:
+            print("Env: ", env['region'], " Toggle: ", env['obj'].toggleOn)
 
 
 class MainApp(App):
